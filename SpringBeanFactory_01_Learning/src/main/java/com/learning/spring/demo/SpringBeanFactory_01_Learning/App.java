1b1df0c5dc897da5fec297f21a7943cd3dadb64f
Sunil Sharma <sunil.sharma@ca.com>
2019-02-26 21:51:47 +0530

    Spring bean demo01

---
+++ SpringBeanFactory_01_Learning/src/main/java/com/learning/spring/demo/SpringBeanFactory_01_Learning/App.java
@@ -0,0 +1,13 @@
+package com.learning.spring.demo.SpringBeanFactory_01_Learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SpringBeanFactory_01_Learning/src/test/java/com/learning/spring/demo/SpringBeanFactory_01_Learning/AppTest.java
@@ -0,0 +1,20 @@
+package com.learning.spring.demo.SpringBeanFactory_01_Learning;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	public void testApp() {
+		assertTrue(true);
+	}
+}
